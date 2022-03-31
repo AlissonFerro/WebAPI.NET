17fc57f562141fa4f8cb93d9758ae6859f1b8bb2
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2022-03-30 21:09:31 -0300

    Inicio da criação webapp

---
+++ Model/Usuario.cs
@@ -0,0 +1,34 @@
+﻿namespace Model;
+public class Usuario
+{
+    private string login;
+    private string password;
+
+    public Usuario(string login, string password)
+    {  
+        this.login = login;
+        this.password = password;
+    }
+
+    public string GetLogin()
+    {
+        return this.login;
+    }
+
+    public void SetLogin(string login)
+    {
+        this.login=login;
+    }
+
+    public string GetPassword()
+    {
+        return this.password;
+    }
+
+    public void SetPassword(string password)
+    {
+        this.password=password;
+    }
+
+
+}
